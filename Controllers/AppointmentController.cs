f8d831d71b1287bba89577acfb142a8e4251350b
JavvajiGayatri <gayatrijavvaji123@gmail.com>
2024-11-04 17:12:01 +0530

    create appointment
    create appointment controller

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,75 @@
+﻿using HospitaManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HospitaManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentController : ControllerBase
+    {
+        private readonly HospitalmgtContext _context;
+
+        public AppointmentController(HospitalmgtContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointments()
+        {
+            return Ok(await _context.Appoinments.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddAppointment([FromBody] Appoinment appointment)
+        {
+            _context.Appoinments.Add(appointment);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetAppointment), new { id = appointment.AppointmentId }, appointment);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAppointment(int id)
+        {
+            var appointment = await _context.Appoinments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            return Ok(appointment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAppointment(int id, [FromBody] Appoinment appointment)
+        {
+            if (id != appointment.AppointmentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(appointment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAppointment(int id)
+        {
+            var appointment = await _context.Appoinments.FindAsync(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+
+            _context.Appoinments.Remove(appointment);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+    }
+}
