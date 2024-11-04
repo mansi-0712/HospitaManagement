590142f436cf3d6720315944a2881ede1aecd665
spoorthybalagar <balagarspoorthy@gmail.com>
2024-11-04 16:51:03 +0530

    Patient controller
    Patient controller

---
+++ Controllers/PatientsController .cs
@@ -0,0 +1,76 @@
+﻿using HospitaManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HospitaManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        // GET: api/<PatientsController>
+        private readonly HospitalmgtContext _context;
+
+        public PatientsController(HospitalmgtContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPatients()
+        {
+            return Ok(await _context.Patients.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPatient([FromBody] Patient patient)
+        {
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetPatient), new { id = patient.PatientId }, patient);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdatePatient(int id, [FromBody] Patient patient)
+        {
+            if (id != patient.PatientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePatient(int id)
+        {
+            var patient = await _context.Patients.FindAsync(id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patients.Remove(patient);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
+
